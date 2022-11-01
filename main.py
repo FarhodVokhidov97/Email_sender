d9890a663b2c216a50ce735ff3b89719a3bbd62f
vohidov farhod <farhodg7463@gmail.com>
2022-11-01 12:18:06 +0500

    setting email

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import ssl
 from email.message import EmailMessage
 
 
-email_sender = '******************'
-email_password = '*************'
+email_sender = '******************'  #Your email address
+email_password = '*************' # Your app passwords
 
-email_receiver = 'farhodg7463@gmail.com'
+email_receiver = '************' #mail the one you want to send
 
 subject = "Dont forget  to subscribe"
 
@@ -19,6 +19,7 @@ em['To'] = email_receiver
 em['subject'] = subject
 em.set_content(body)
 
+# setting with google smtp
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
     smtp.login(email_sender,email_password)
